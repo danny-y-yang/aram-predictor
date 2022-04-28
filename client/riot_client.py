41edf143b14d6d153cf203fe0b6ff286ff1071d9
Rodger <pangrodger@gmail.com>
2022-04-27 17:48:52 -0700

    Implemented data parser, now throws exception if missing api key

--- client/riot_client.py
+++ client/riot_client.py
@@ -1,22 +1,115 @@
 import requests
 import os
+import pandas as pd
+import numpy as np
+import json
 
-elu_puuid = "XmEmjOPvqoEUkkgks49W3gYvvumXWcibTYtuSYHgDGSL_w85_ZzwsOPZKtLABf-YiRK2bNNai-2IrA"
+#checks if riot_api_key exists in environment variables
+if ("riot_api_key" not in os.environ):
+	raise Exception("Riot API key does not exist. Please set a key as riot_api_key = your_key in environment variables")
+else:
+	dev_key = os.environ["riot_api_key"]
 
-dev_key = os.environ["riot_api_key"]
-
-#Get player id by summoner name
-def get_player_id(summoner_name):
+#Get player ids by summoner name
+def get_player_ids(summoner_name):
 	api_name = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"
 	response = requests.get(api_name + summoner_name + "?api_key=" + dev_key)
-	print(response.json())
+	return response.json()
+
+#Get puuid by summoner name
+def get_puuid(summoner_name):
+	api_name = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"
+	response = requests.get(api_name + summoner_name + "?api_key=" + dev_key)
+	return response.json().get("puuid")
+
+#Get a summoner name by puuid, like XmEmjOPvqoEUkkgks49W3gYvvumXWcibTYtuSYHgDGSL_w85_ZzwsOPZKtLABf-YiRK2bNNai-2IrA
+def get_summoner_name(puuid):
+	api_name = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/"
+	response = requests.get(api_name + puuid + "?api_key=" + dev_key)
+	data = response.json()
+	return data.get("name")
 
 #Get match history by puuid, returns a list of match IDs
-def get_matches(puuid, start = 0, count = 20):
+def get_match_ids(puuid, start = 0, count = 20):
 	api_name = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/"
 	response = requests.get(api_name + puuid + "/ids?start=" + str(start) + "&count=" + str(count) + "&api_key=" + dev_key)
-	print(response.json()) 
+	return response.json()
 
-get_player_id("Elucidaze")
+#pull rune data
+def get_rune_info():
+	api_name = "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/perks.json"
+	response = requests.get(api_name)
+	data = response.json()
+	df = pd.DataFrame.from_dict(data)
+	df.to_csv("./runes_info.csv", index = False)
 
-get_matches(elu_puuid, 0, 100)
+#pull summoner spell data
+def get_summoner_spells():
+	api_name = "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/summoner-spells.json"
+	response = requests.get(api_name)
+	data = response.json()
+	df = pd.DataFrame.from_dict(data)
+	df.to_csv("./summoner_spells_info.csv", index = False)
+
+#simplify concatenation syntax
+def concatenate(df, df_new):
+	return pd.concat([df, df_new])
+
+#gets information for a match by match id
+def get_match(match_id):
+	api_name = "https://americas.api.riotgames.com/lol/match/v5/matches/"
+	response = requests.get(api_name + str(match_id) + "?api_key=" + dev_key)
+	data = response.json()
+
+	# df = pd.DataFrame.from_dict(data.get("info").get("participants"))
+	df = pd.json_normalize(data, record_path = ['info', 'participants'], meta = [['metadata', 'gameMode']])
+
+	#Perks (runes) are in a nested JSON format. I normalize the data using a defined path and then reshape for joining
+	df_perks = pd.json_normalize(data, record_path = ['info', 'participants', 'perks', 'styles', 'selections'])
+	df_perks = df_perks["perk"]
+	df_perks = pd.DataFrame(df_perks.values.reshape(-1, 6), columns=['perk1','perk2','perk3','perk4','perk5', 'perk6'])
+
+	df["matchId"] = match_id
+	return df.join(df_perks)
+
+#Creates a csv of match data by summoner name. Future plan is to generate a bunch of match ids, then use that set of match ids as argument only
+def generate_match_data(summoner_name, match_count):
+	matches_generated = []
+	my_puuid = get_puuid(summoner_name)
+	match_ids = get_match_ids(my_puuid, 0, match_count)
+	df_all = pd.DataFrame()
+
+
+	for match_id in match_ids:
+		#Checks if the match has already been generated. We do not want to concatenate duplicate matches
+		if match_id not in matches_generated:
+			df_all = concatenate(df_all, get_match(match_id))
+			matches_generated.append(match_id)
+
+	#These are all the features we will need. Moving relevant columns to the front
+	cols_to_move = [
+		"matchId",
+		"championId",
+		"championName",
+		"metadata.gameMode",
+		"participantId",
+		"perk1",
+		"perk2",
+		"perk3",
+		"perk4",
+		"perk5",
+		"perk6",
+		"puuid",
+		"summoner1Id",
+		"summoner2Id",
+		"summonerId",
+		"summonerLevel",
+		"summonerName",
+		"teamId",
+		"win"
+	]
+
+	df_all = df_all[cols_to_move + [col for col in df_all.columns if col not in cols_to_move]]
+	df_all.to_csv("./match_" + str(match_count) + ".csv", index = False)
+
+generate_match_data("Elucidaze", 50)
